a3a75a80ca6d8ca316288a72c7953b5326db21e1
tsalm0n <tsalm0n@twc.com>
2019-09-13 15:12:48 -0400

    reestablish jawsdb_maria setup

--- routes/rt-editEntryPassport.js
+++ routes/rt-editEntryPassport.js
@@ -2,15 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 var mysql = require('mysql')
-var connection = mysql.createConnection({ //old - from local db setup
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'catRelTrkr'
-});
+// var connection = mysql.createConnection({ //old - from local db setup
+//   host: 'localhost',
+//   user: 'root',
+//   password: '',
+//   database: 'catRelTrkr'
+// });
 
-// const connection = mysql.createConnection(process.env.JAWSDB_MARIA_URL);
-// connection.connect();
+const connection = mysql.createConnection(process.env.JAWSDB_MARIA_URL);
+connection.connect();
 
 /* GET db-input page. */
 router.get('/', function (req, res, next) {
