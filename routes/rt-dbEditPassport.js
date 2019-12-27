a643419ab5243db8c8b0fe7fc5816989912bb1a9
tsalm0n <tsalm0n@twc.com>
2019-12-27 11:08:55 -0500

    add sort by asc to vendorName column

--- routes/rt-dbEditPassport.js
+++ routes/rt-dbEditPassport.js
@@ -94,7 +94,7 @@ router.post('/results', (req, res, next) => { //take POST request data from dbEd
 
   if (formInput1 == '' && formInput2 == '' && formInput3 == '' && formInput4 == '' &&
     formInput5 == '' && formInput6 == '' && formInput7 == '' && formInput8 == '') { //return all table entries if search string is empty
-    connection.query("SELECT * FROM rainbowcat", function (err, rows, fields) {
+    connection.query(`SELECT * FROM rainbowcat ORDER BY vendorName ASC`, function (err, rows, fields) {
       if (err) throw err;
       showSearchResults(rows);
 
@@ -107,14 +107,9 @@ router.post('/results', (req, res, next) => { //take POST request data from dbEd
     if (formInput0 !== undefined && formInput1 !== undefined && formInput2 !== undefined &&
       formInput3 !== undefined && formInput4 !== undefined && formInput5 !== undefined &&
       formInput6 !== undefined && formInput7 !== undefined && formInput8 !== undefined) {
-      connection.query("SELECT * FROM rainbowcat WHERE vendorName LIKE " + "'" + formInput1 + "%" + "'" +
-        " AND ediName LIKE " + "'" + formInput2 + "%" + "'" +
-        " AND issueDate LIKE " + "'" + formInput3 + "%" + "'" +
-        " AND needNewCat LIKE " + "'" + formInput4 + "%" + "'" +
-        " AND updatedWLatest LIKE " + "'" + formInput5 + "%" + "'" +
-        " AND reporter LIKE " + "'" + formInput6 + "%" + "'" +
-        " AND comments LIKE " + "'" + formInput7 + "%" + "'" +
-        " AND andrea LIKE " + "'" + formInput8 + "%" + "'",
+      connection.query(`SELECT * FROM rainbowcat WHERE vendorName LIKE '${formInput1}%' AND 'ediName' LIKE '${formInput2}%' 
+			AND ${issueDate} LIKE '${formInput3}%' AND ${needNewCat} LIKE '${formInput4}%' AND ${updatedWLatest} LIKE '${formInput5}%' 
+			AND ${reporter} LIKE '${formInput6}%' AND ${comments} LIKE '${formInput7}%' ORDER BY vendorName ASC`,
         function (err, rows, fields) {
           if (err) throw err;
           // console.log('rows==>', rows);
