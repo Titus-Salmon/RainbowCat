2d724f23637cc6cfce7c6bb65ec01bab370e555d
tsalm0n <tsalm0n@twc.com>
2020-01-14 12:18:02 -0500

    add new Andrea column; cleanup

--- public/javascripts/editEntryPassport.js
+++ public/javascripts/editEntryPassport.js
@@ -1,24 +1,26 @@
 function populateForm() {
-    let prKyInput = document.getElementById('prKy');
-    let vndNmInput = document.getElementById('vndNm');
-    let ediNmInput = document.getElementById('ediNm');
-    let issuDtInput = document.getElementById('issuDt');
-    let ndNwCtInput = document.getElementById('ndNwCt');
-    let updtwLtstInput = document.getElementById('updtwLtst');
-    let rptrInput = document.getElementById('rptr');
-    let cmntInput = document.getElementById('cmnt');
-    let andrInput = document.getElementById('andr');
-    let vndEmlInput = document.getElementById('vndEml');
+    let prKyInput = document.getElementById('prKy')
+    let vndNmInput = document.getElementById('vndNm')
+    let ediNmInput = document.getElementById('ediNm')
+    let issuDtInput = document.getElementById('issuDt')
+    let ndNwCtInput = document.getElementById('ndNwCt')
+    let updtwLtstInput = document.getElementById('updtwLtst')
+    let cmnt1Input = document.getElementById('cmnt1')
+    let cmnt2Input = document.getElementById('cmnt2')
+    let cmnt3Input = document.getElementById('cmnt3')
+    let andrInput = document.getElementById('andr')
+    let vndEmlInput = document.getElementById('vndEml')
 
-    prKyInput.value = JSON.parse(localStorage.getItem("clickedRowData"))[0];
-    vndNmInput.value = JSON.parse(localStorage.getItem("clickedRowData"))[1];
-    ediNmInput.value = JSON.parse(localStorage.getItem("clickedRowData"))[2];
-    issuDtInput.value = JSON.parse(localStorage.getItem("clickedRowData"))[3];
-    ndNwCtInput.value = JSON.parse(localStorage.getItem("clickedRowData"))[4];
-    updtwLtstInput.value = JSON.parse(localStorage.getItem("clickedRowData"))[5];
-    rptrInput.value = JSON.parse(localStorage.getItem("clickedRowData"))[6];
-    cmntInput.value = JSON.parse(localStorage.getItem("clickedRowData"))[7];
-    andrInput.value = JSON.parse(localStorage.getItem("clickedRowData"))[8];
-    vndEmlInput.value = JSON.parse(localStorage.getItem("clickedRowData"))[9];
+    prKyInput.value = JSON.parse(localStorage.getItem("clickedRowData"))[0]
+    vndNmInput.value = JSON.parse(localStorage.getItem("clickedRowData"))[1]
+    ediNmInput.value = JSON.parse(localStorage.getItem("clickedRowData"))[2]
+    issuDtInput.value = JSON.parse(localStorage.getItem("clickedRowData"))[3]
+    ndNwCtInput.value = JSON.parse(localStorage.getItem("clickedRowData"))[4]
+    updtwLtstInput.value = JSON.parse(localStorage.getItem("clickedRowData"))[5]
+    cmnt1Input.value = JSON.parse(localStorage.getItem("clickedRowData"))[6]
+    cmnt2Input.value = JSON.parse(localStorage.getItem("clickedRowData"))[7]
+    cmnt3Input.value = JSON.parse(localStorage.getItem("clickedRowData"))[8]
+    andrInput.value = JSON.parse(localStorage.getItem("clickedRowData"))[9]
+    vndEmlInput.value = JSON.parse(localStorage.getItem("clickedRowData"))[10]
 }
-populateForm();
+populateForm()
