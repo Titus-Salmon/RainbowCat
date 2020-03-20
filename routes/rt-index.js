14656ee2cffacb26ac666909828e2c83cd21ba77
tsalm0n <tsalm0n@twc.com>
2020-03-19 20:24:31 -0400

    cleanup

--- routes/rt-index.js
+++ routes/rt-index.js
@@ -9,7 +9,6 @@ const {
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('vw-index', {
-    // title: 'vw-index'
     title: 'vw-index'
   });
 });
@@ -22,35 +21,7 @@ router.get('/dashboard', ensureAuthenticated, function (req, res, next) {
     userEmail: req.user.email,
     userEmail_stringified: JSON.stringify(req.user.email),
   });
-  // console.log('before if (req.user.email)')
-  // if (req.user.email !== 'tsalm0n@twc.com') {
-  //   // document.getElementsByClassName('admin').disabled = true;
-  //   // document.getElementsByClassName('admin').backgroundColor = 'red';
-  //   console.log('this is not an admin!')
-  // }
-  // console.log('req.user==>', req.user)
-  // console.log('req.user.email==>', req.user.email)
-
 });
 
-// router.get('/login', passport.authenticate('github'));
-
-// router.get('/auth_t0d', passport.authenticate('github'));
-
-// router.get('/auth_t0d', passport.authenticate('github', {
-//   successRedirect: '/',
-//   failureRedirect: '/loginFailed'
-// }));
-
-// router.get('/Andrea',
-//   require('connect-ensure-login').ensureLoggedIn(),
-//   function (req, res) {
-//     res.render('vw-andrea', {
-//       user: req.user
-//     });
-//   });
-
-// router.get('/Andrea',
-//   require('connect-ensure-login').ensureLoggedIn());
 
 module.exports = router;
